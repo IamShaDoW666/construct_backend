efd81d527e4099f58236769cedeba36bdf8ab16c
nibhapr <nibhapr@gmail.com>
2025-04-16 23:19:43 +0530

    add delete + update feature

--- src/controllers/mediaController.ts
+++ src/controllers/mediaController.ts
@@ -3,6 +3,8 @@ import { Response, Request } from "express";
 import { sendError, sendSuccess } from "../utils/network";
 import { ImageType } from "@prisma/client";
 import { createReference } from "../utils/common";
+import path from "path";
+import fs from "fs/promises";
 
 const getAllMedia = async (req: Request, res: Response) => {
   const media = await db.media.findMany({
@@ -35,15 +37,14 @@ const getMediaById = async (req: Request, res: Response) => {
   sendSuccess(res, media);
 };
 
-const createBulkMedia = async (req: Request, res: Response) => {    
+const createBulkMedia = async (req: Request, res: Response) => {
   if (!req.files) {
-    res.status(400).json({ error: "No files uploaded" });
+    sendError(res, "No files uploaded", 400);
     return;
   }
 
   const files = req.files as Express.Multer.File[];
-  const ref = createReference(req.body.reference)
-  
+  const ref = createReference(req.body.reference);
 
   try {
     const batch = await db.batch.create({
@@ -54,9 +55,9 @@ const createBulkMedia = async (req: Request, res: Response) => {
           connect: {
             id: req.user!.id,
           },
-        }
+        },
       },
-    });   
+    });
 
     const mediaData = files.map((file) => ({
       title: file.filename,
@@ -72,6 +73,103 @@ const createBulkMedia = async (req: Request, res: Response) => {
     });
 
     sendSuccess(res, media, "Media uploaded successfully", 201);
+  } catch (error) {
+    sendError(res, `Error uploading media ${error}`, 500);
+  }
+};
+
+const deleteBulkMedia = async (req: Request, res: Response) => {
+  const { mediaIds } = req.body;
+  console.log("mediaIds", mediaIds);
+  if (!mediaIds || mediaIds.length === 0) {
+    sendError(res, "No media IDs provided", 400);
+    return;
+  }
+  const mediaRecords = await db.media.findMany({
+    where: {
+      id: {
+        in: mediaIds,
+      },
+    },
+  });
+  const deleteFilePromises = mediaRecords.map(async (media) => {
+    const filePath = path.resolve(media.url); // Adjust if 'filepath' includes full path
+    try {
+      await fs.unlink(filePath);
+      await db.media.delete({
+        where: {
+          id: media.id,
+        },
+      });
+    } catch (err) {
+      console.error(`Failed to delete file: ${filePath}`, err);
+      // Optionally handle specific errors, e.g., file not found
+    }
+  });
+
+  await Promise.all(deleteFilePromises);
+  sendSuccess(res, mediaIds, "Media deleted successfully", 204);
+};
+
+const updateBulkMedia = async (req: Request, res: Response) => {
+  console.log("res", req.body);
+  console.log("files", req.files);
+  if (!req.files && !req.body.reference) {
+    sendError(res, "No files uploaded", 400);
+    return;
+  }
+
+  if (!req.body.batchId) {
+    sendError(res, "Batch ID is required", 400);
+    return;
+  }
+
+  const batch = await db.batch.findUnique({
+    where: {
+      id: req.body.batchId,
+    },
+  });
+
+  if (!batch) {
+    res.status(404).json({ error: "Batch not found" });
+    return;
+  }
+
+  const files = req.files as Express.Multer.File[];
+  let ref = batch.reference;
+  if (batch.reference !== req.body.reference) {
+    ref = batch.reference?.slice(0, 7) + req.body.reference;
+  }
+
+  try {
+    const response = await db.batch.update({
+      data: {
+        reference: ref,
+      },
+      where: {
+        id: batch.id,
+      },
+    });
+
+    const mediaData = files.map((file) => ({
+      title: file.filename,
+      type: ImageType.UPLOAD,
+      reference: ref + Math.random().toString(36).slice(2, 5),
+      url: file.path,
+      uploadedUserId: req.user!.id,
+      batchId: batch.id,
+    }));
+
+    await db.media.createMany({
+      data: mediaData,
+    });
+
+    const responseData = await db.media.findMany({
+      where: {
+        batchId: batch.id,
+      },
+    });
+    sendSuccess(res, responseData, "Media uploaded successfully", 201);
   } catch (error) {
     sendError(res, `Error uploading media ${error}`, 500);
   }
@@ -106,7 +204,7 @@ const createMedia = async (req: Request, res: Response) => {
                 },
               },
             },
-          }
+          },
         },
         uploadedUser: {
           connect: {
@@ -136,7 +234,9 @@ const mediaController = {
   getAllMedia,
   createMedia,
   createBulkMedia,
+  updateBulkMedia,
   deleteMedia,
+  deleteBulkMedia,
 };
 
 export default mediaController;
--- src/routes/mediaRouter.ts
+++ src/routes/mediaRouter.ts
@@ -1,9 +1,15 @@
-import express from 'express';
-import mediaController from '../controllers/mediaController';
-import upload from '../utils/multer';
+import express from "express";
+import mediaController from "../controllers/mediaController";
+import upload from "../utils/multer";
 const router = express.Router();
 
-router.get('/', mediaController.getAllMedia);
-router.post('/', upload.single('image'), mediaController.createMedia)
-router.post('/bulk', upload.array('images'), mediaController.createBulkMedia);
+router.get("/", mediaController.getAllMedia);
+router.post("/", upload.single("image"), mediaController.createMedia);
+router.post("/bulk", upload.array("images"), mediaController.createBulkMedia);
+router.post(
+  "/bulk/update",
+  upload.array("images"),
+  mediaController.updateBulkMedia
+);
+router.delete("/bulk/delete", mediaController.deleteBulkMedia);
 export default router;
