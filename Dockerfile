06886d6685a8d01642467381f24dd64295dba6c9
nibhapr <nibhapr@gmail.com>
2025-04-17 22:30:15 +0530

    dockerize

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+# Use the official Node.js LTS Alpine image for a lightweight build
+FROM node:lts-alpine
+
+# Set the working directory inside the container
+WORKDIR /app
+
+# Copy package.json and package-lock.json to install dependencies
+COPY package*.json ./
+COPY pnpm-lock.yaml ./
+
+# Install dependencies
+RUN npm install -g pnpm@latest
+RUN pnpm install --frozen-lockfile
+
+# Copy the rest of the application code
+COPY . .
+
+# Generate Prisma client
+RUN pnpx prisma db push
+RUN pnpx prisma generate
+
+# Build the TypeScript code
+RUN pnpm build
+RUN ls -l /app
+
+
+# Expose the port the app runs on
+EXPOSE 3303
--- src/utils/network.ts
+++ src/utils/network.ts
@@ -1,8 +1,13 @@
 import { Response } from "express";
-import type { BaseResponse } from "@/types";
+import type { BaseResponse } from "../types";
 
 // Success Response
-export function sendSuccess<T>(res: Response, data: T, message = "Success", statusCode = 200) {
+export function sendSuccess<T>(
+  res: Response,
+  data: T,
+  message = "Success",
+  statusCode = 200
+) {
   const response: BaseResponse<T> = {
     status: true,
     statusCode,
@@ -13,7 +18,12 @@ export function sendSuccess<T>(res: Response, data: T, message = "Success", stat
 }
 
 // Error Response
-export function sendError(res: Response, message = "Something went wrong", statusCode = 500, error?: any) {
+export function sendError(
+  res: Response,
+  message = "Something went wrong",
+  statusCode = 500,
+  error?: any
+) {
   const response: BaseResponse = {
     status: false,
     statusCode,
