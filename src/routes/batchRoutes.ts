2eb36b13943c6525f34237c18335a1a907fc51db
IamShaDoW666 <www.gaming2018@gmail.com>
2025-04-21 22:17:49 +0530

    add batch delete endpoint

--- src/routes/batchRoutes.ts
+++ src/routes/batchRoutes.ts
@@ -4,5 +4,6 @@ import upload from '../utils/multer';
 const router = express.Router();
 
 router.get('/', batchController.getAllBatch);
+router.delete('/:id', batchController.deleteBatch);
 router.post('/', batchController.createBatch);
 export default router;
